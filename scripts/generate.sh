da741231fdb80f0f8064c988edb4b6d536a9e560
Yoni Friedman <yyf@mit.edu>
2021-12-14 23:03:04 -0500

    Cleaned up some files

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name blender_rendering
+#SBATCH --job-name blendering
 #SBATCH --mail-type=END,FAIL,BEGIN
 #SBATCH --mail-user=yyf@mit.edu
 #SBATCH -t 48:15:00
@@ -9,6 +9,6 @@
 #SBATCH -p tenenbaum
 #SBATCH --mem=5G
 
-Blender/blender -b -noaudio -P generate_scenes.py -- --root_dir /om2/user/yyf/CommonFate/scenes/voronoi_single_shape/ --n_scenes 10000 --start_scene 280 --render_size 512 --render_views masks --scene_config configs/1_shape_textured_background.json --engine CYCLES --n_frames 64 --samples 64 --device CUDA
+Blender/blender -b -noaudio -P generate_scenes.py -- --root_dir /om2/user/yyf/CommonFate/scenes/voronoi_single_shape/ --n_scenes 3000 --start_scene 6676 --render_size 512 --render_views masks --scene_config configs/1_shape_textured_background.json --engine CYCLES --n_frames 64 --samples 64 --device CUDA
 
 
